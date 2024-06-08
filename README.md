0487af3ba4dc6b3a032400f778952a6a75d0db34
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-06-09 01:03:45 +0200

    feat: add vm template config

--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 
 My messy nix config with flakes !
 
-Currently managed hosts :
+Currently managed configs :
 - Laptop workstation
 - Raspberry Pi 3 home-assistant instance
+- NixOS qemu VM templates with cloud-init (only for initial setup)
 
 ## Deploying config to Raspberry Pi 3
 
 ```shell
 NIX_SSHOPTS="-t" nixos-rebuild switch --flake ./hosts/pi3#rpi --target-host ops@nix-pi301.skynet.local --use-remote-sudo
 ```
+
+## Building VM template images
+
+Based on [flake from voidus](https://gist.github.com/voidus/1230b200043b7f815e2513663d16353b)
+
+```shell
+nix build ./hosts/vmTemplates/aarch64#image
+```
+
+> If building from x86 host this usually takes around 40 minutes.
---
+++ hosts/vmTemplates/aarch64/flake.nix
@@ -0,0 +1,123 @@
+{
+  description = "A nixos cloudinit base image without nixos-infect";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "aarch64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    inherit (pkgs) lib;
+
+    baseModule = { lib, config, pkgs, ...}: {
+      nixpkgs.hostPlatform = "aarch64-linux";
+      imports = [
+        "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+      ];
+
+      fileSystems = {
+        "/boot" = {
+          label = "esp";
+          device = "/dev/disk/by-label/ESP";
+          fsType = "vfat";
+        };
+        "/" = {
+          label = "nixos";
+          device = "/dev/disk/by-label/nixos";
+          autoResize = true;
+          fsType = "ext4";
+        };
+      };
+
+      boot = {
+        tmp.cleanOnBoot = true; growPartition = true;
+        loader = {
+          systemd-boot.enable = true;
+          efi = {
+            canTouchEfiVariables = true;
+          };
+          grub = {
+            device = lib.mkDefault "/dev/vda";
+          };
+        };
+      };
+
+      services.openssh.enable = true;
+
+      services.qemuGuest.enable = true;
+
+      security.sudo.wheelNeedsPassword = false;
+
+      users.users.ops = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+      };
+
+      networking = {
+        hostName = "nixos-cloudinit";
+      };
+
+      systemd.network.enable = true;
+
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
+      services.cloud-init = {
+        enable = true;
+        network.enable = true;
+        config = ''
+          system_info:
+            distro: nixos
+            network:
+              renderers: [ 'networkd' ]
+            default_user:
+              name: ops
+          users:
+              - default
+          ssh_pwauth: true
+          chpasswd:
+            expire: false
+          cloud_init_modules:
+            - migrator
+            - seed_random
+            - bootcmd
+            - write-files
+            - growpart
+            - resizefs
+            - update_hostname
+          cloud_config_modules:
+            - disk_setup
+            - mounts
+            - set-passwords
+            - runcmd
+            - ssh
+          cloud_final_modules:
+            - rightscale_userdata
+            - ssh-authkey-fingerprints
+          runcmd:
+            - [ sh, -c, 'sudo nixos-rebuild switch -I nixos-config=/etc/nixos/configuration.nix | tee -a /tmp/runcmd.log;sudo reboot now' ]
+        '';
+      };
+    };
+
+    nixos = nixpkgs.lib.nixosSystem {
+      modules = [baseModule];
+    };
+
+    make-disk-image = import "${nixpkgs}/nixos/lib/make-disk-image.nix";
+  in {
+    inherit pkgs;
+    image = make-disk-image {
+      inherit pkgs lib;
+      config = nixos.config;
+      name = "nixos-cloudinit";
+      format = "qcow2";
+      partitionTableType = "efi";
+      copyChannel = true;
+    };
+  };
+}
