3b5ef8b3336ebd5063dec195101680a4f5f836c8
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-06-09 01:26:14 +0200

    git: add nix config & gpg key

--- flake.nix
+++ flake.nix
@@ -140,11 +140,24 @@
                 };
                 git = {
                   enable = true;
-                  package = pkgs.gitAndTools.gitFull; # Install git wiith all the optional extras
-                  userName = "KongrooParadox";
-                  userEmail = "kongroo.git@proton.me";
+                  package = pkgs.gitAndTools.gitFull;
+                  userName = "Guillaume Nanty";
+                  userEmail = "7790572+KongrooParadox@users.noreply.github.com";
+                  signing = {
+                    signByDefault = true;
+                    gpgPath = "/run/current-system/sw/bin/gpg";
+                    key = "E1FFAC5C39F79113";
+                  };
+                  lfs.enable = true;
                   extraConfig = {
-                    core.editor = "nvim";
+                    core = {
+                      editor = "nvim";
+                      autocrlf = false;
+                      whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
+                      excludesfile = "~/.gitignore";
+                    };
+                    help.autocorrect = 20;
+                    init.defaultBranch = "main";
                     credential.helper = "cache";
                   };
                 };
