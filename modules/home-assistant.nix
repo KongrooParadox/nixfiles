5d5f02e91ddc1b1f40da848e3e1f82ef9f4e49e8
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-06-13 02:17:08 +0200

    asgard vm

---
+++ modules/home-assistant.nix
@@ -0,0 +1,45 @@
+{
+  imports = [];
+
+  # options = {
+  #
+  # };
+  config = {
+    services = {
+      mosquitto = {
+        enable = true;
+        listeners = [
+        {
+          users.mosquitto = {
+            acl = [
+              "readwrite #"
+            ];
+            hashedPassword = "$7$101$zZowRHVB/HcjcgnJ$q4L1Vgw+riu3UBpBpjSPzS6P0hOrodx/bf1lWqEH5TR0aqYz2sl71eG04ksY/II98rGi1kFHndS9O3KsNLrbtw==";
+          };
+        }
+        ];
+      };
+      home-assistant = {
+        enable = true;
+        extraComponents = [
+          "esphome"
+          "met"
+          "radio_browser"
+          "mqtt"
+          "tasmota"
+          "google_translate"
+          "freebox"
+        ];
+        config = {
+          default_config = {};
+          mqtt = {};
+        };
+      };
+    };
+    security.sudo.wheelNeedsPassword = false;
+    networking = {
+      firewall.allowedTCPPorts = [ 1883 8123 22 ];
+    };
+
+  };
+}
