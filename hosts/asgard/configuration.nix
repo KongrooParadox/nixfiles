4b280e176c46a16d12f0fdb269414809710c65e5
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-10-06 18:34:31 +0200

    feat: dns server on asgard
    mv asgard to pi3

---
+++ hosts/asgard/configuration.nix
@@ -0,0 +1,49 @@
+{ config, inputs, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    git
+    tmux
+    vim
+  ];
+
+  boot = {
+    tmp.cleanOnBoot = true; growPartition = true;
+    loader = {
+      generic-extlinux-compatible.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      grub = {
+        enable = false;
+        device = lib.mkDefault "/dev/vda";
+      };
+    };
+  };
+
+  services.openssh.enable = true;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  users.users.ops = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      (builtins.readFile ~/.ssh/id_ed25519.pub)
+    ];
+  };
+
+  networking = {
+    hostName = "asgard";
+    networkmanager.enable = true;
+  };
+
+  nix.settings = {
+    trusted-users = [ "root" "ops" ];
+    experimental-features = [
+      "nix-command"
+        "flakes"
+    ];
+  };
+  system.stateVersion = "24.05";
+}
