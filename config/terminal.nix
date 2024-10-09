d899d32cea0d128b44fd608e340857a4d9ecd2c8
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-10-10 01:47:57 +0200

    feat: nvim / tmux navigation

--- config/terminal.nix
+++ config/terminal.nix
@@ -37,6 +37,14 @@
       terminal = "screen-256color";
       clock24 = true;
       baseIndex = 1;
+      aggressiveResize = true;
+      plugins = with pkgs; [
+        tmuxPlugins.vim-tmux-navigator
+        {
+          plugin = tmuxPlugins.resurrect;
+          extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        }
+      ];
       extraConfig = ''
         set -g status-style 'bg=#333333 fg=#5eacd3'
         bind -T copy-mode-vi v send-keys -X begin-selection
@@ -53,6 +61,24 @@
         bind-key -n M-o run-shell "tmux-switcher ~/personal/zellij"
         bind -n Insert next-window
         bind r source-file ~/.config/tmux/tmux.conf
+        # Focus events enabled for terminals that support them
+        set -g focus-events on
+        is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+          | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+        bind-key -n 'M-h' if-shell "$is_vim" 'send-keys M-h'  'select-pane -L'
+        bind-key -n 'M-j' if-shell "$is_vim" 'send-keys M-j'  'select-pane -D'
+        bind-key -n 'M-k' if-shell "$is_vim" 'send-keys M-k'  'select-pane -U'
+        bind-key -n 'M-l' if-shell "$is_vim" 'send-keys M-l'  'select-pane -R'
+        tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+        if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+            "bind-key -n 'M-\\' if-shell \"$is_vim\" 'send-keys M-\\'  'select-pane -l'"
+        if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+            "bind-key -n 'M-\\' if-shell \"$is_vim\" 'send-keys M-\\\\'  'select-pane -l'"
+        bind-key -T copy-mode-vi 'M-h' select-pane -L
+        bind-key -T copy-mode-vi 'M-j' select-pane -D
+        bind-key -T copy-mode-vi 'M-k' select-pane -U
+        bind-key -T copy-mode-vi 'M-l' select-pane -R
+        bind-key -T copy-mode-vi 'M-\' select-pane -l
         '';
     };
     zsh = {
