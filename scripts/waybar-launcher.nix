0ff8fff4ef96c50c7fc7e9898c07107c58cce68a
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-10-18 07:49:54 +0200

    add wrapper script & hyprland binding for waybar

---
+++ scripts/waybar-launcher.nix
@@ -0,0 +1,11 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "waybar-launcher" ''
+  if pgrep "waybar-wrapped" > /dev/null; then
+    # Waybar is running, kill it
+    pkill waybar-wrapped
+  fi
+  sleep 0.2
+  waybar
+''
+
