ceff8b615154249f37a3c6d1e68e4554840a5123
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-11-30 07:43:56 +0100

    style: tidy asgard

--- hosts/asgard/configuration.nix
+++ hosts/asgard/configuration.nix
@@ -21,14 +21,16 @@
     };
   };
 
-  services.openssh = {
-    enable = true;
-    hostKeys = [
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-    ];
+  services = {
+    openssh = {
+      enable = true;
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+    };
   };
 
   security.sudo.wheelNeedsPassword = false;
