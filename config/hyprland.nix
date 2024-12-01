99cc5d7ce85434a7881fef36bb89aa6c75f0f214
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-12-01 08:55:47 +0100

    baldur: upgrade to 24.11 release

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -99,10 +99,6 @@ with lib;
           }
           decoration {
             rounding = 10
-            drop_shadow = true
-            shadow_range = 4
-            shadow_render_power = 3
-            col.shadow = rgba(1a1a1aee)
             blur {
                 enabled = true
                 size = 5
