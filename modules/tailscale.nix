1e10f16d3ba6bbf0d539a789a835d808b677ff0d
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-12-10 06:34:42 +0100

    feat: tailscale exit node

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -3,7 +3,11 @@
     tailscale =
     {
       enable =
-        lib.mkEnableOption "enables tailscale module";
+        lib.mkEnableOption "Enable tailscale module";
+      exitNode = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
       ssh = lib.mkOption {
         type = lib.types.bool;
         default = false;
@@ -19,14 +23,10 @@
     services.tailscale = {
       authKeyFile = lib.mkDefault config.sops.secrets."tailscale/server-key".path;
       enable = lib.mkDefault true;
+      extraSetFlags = [ "--advertise-exit-node=${lib.boolToString(config.tailscale.exitNode)}"];
       extraUpFlags =
         (lib.optional config.tailscale.ssh "--ssh") ++
         (lib.optional config.tailscale.subnetRouter "--advertise-routes=10.10.1.0/24");
-      #
-      # extraUpFlags = [
-      #   "--ssh"
-      #   # "--advertise-routes=10.10.1.100/24"
-      # ];
       openFirewall = lib.mkDefault true;
       useRoutingFeatures = lib.mkDefault "both";
     };
