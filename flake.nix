ea190698a2bc6a4de77cade1d659260b5fed8063
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-01-31 04:11:05 +0100

    baldur -> latest nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "flake for baldur";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
