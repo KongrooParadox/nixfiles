06e08572b5c7d10373d4b6ba7486332049d300c1
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-02-16 01:03:31 +0100

    feat: jellyfin media server

---
+++ modules/nixos/media.nix
@@ -0,0 +1,40 @@
+{ config, lib, ... }:
+let
+  cfg = config.media;
+in
+{
+  options.media = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.mdDoc "Whether to enable the media suite (Jellyfin & co).";
+    };
+
+    hostname = lib.mkOption {
+      type = lib.types.str;
+      example = "my-server.example.org";
+      description = lib.mdDoc ''
+        FQDN Hostname of Jellyfin server.
+        This will be used as the base url for NGINX reverse proxy.
+        '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.groups.media = {};
+
+    reverseProxy = {
+      hostname = cfg.hostname;
+      services = {
+        jellyfin.port = 8096;
+      };
+    };
+
+    services.jellyfin = {
+      enable = true;
+      group = "media";
+      openFirewall = true;
+      dataDir = "/mnt/compute/jellyfin";
+    };
+  };
+}
