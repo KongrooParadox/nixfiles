138e9d7aa6476665bf3cfb45777963928134c7a6
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-02-16 01:11:24 +0100

    update dns hosts

--- hosts/asgard/default.nix
+++ hosts/asgard/default.nix
@@ -33,12 +33,13 @@
       "tasmota-nas.skynet.local" = "10.10.111.27";
       "tasmota-tv.skynet.local" = "10.10.111.14";
       "njord.skynet.local" = "10.10.111.26,10.10.111.31";
-      "baldur.skynet.local" = "10.10.111.20,10.10.111.21,2a01:cb1d:824f:1000:7f0b:431c:2066:a6ce";
-      "asgard.skynet.local" = "10.10.111.100,2a01:cb1d:824f:1000:44a0:293a:69fa:1c5f";
-      "yggdrasil.skynet.local" = "10.10.111.101,2a01:cb1d:824f:1000:24d1:69f:4b2c:78b9";
+      "baldur.skynet.local" = "10.10.111.20,10.10.111.21";
+      "asgard.skynet.local" = "10.10.111.100";
+      "yggdrasil.skynet.local" = "10.10.111.101";
       "heimdall.skynet.local" = "10.10.111.102";
       "kronos.skynet.local" = "10.10.111.103";
       "thor.skynet.local" = "10.10.111.104";
+      "pi401.skynet.local" = "10.10.111.123";
       "livebox.skynet.local" = "10.10.111.254";
     };
   };
