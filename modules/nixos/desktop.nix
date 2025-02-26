f4d9f4c898c2b141926e96fa15da376fdf195196
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-02-26 23:12:27 +0100

    fix some gtk4 apps (pavucontrol)

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -14,55 +14,58 @@ in
   config = lib.mkIf cfg.enable {
     system.stateVersion = stateVersion;
 
-    environment.systemPackages = with pkgs; [
-      adwaita-icon-theme
-      android-tools
-      buildah
-      calibre
-      deluge-gtk
-      #discord #broken for aarch64
-      displaylink
-      element-desktop
-      evolution
-      gimp#-with-plugins # broken for aarch64
-      gnupg
-      go
-      helmfile
-      hugo
-      hyprpicker
-      ifuse # optional, to mount using 'ifuse'
-      inkscape
-      krita
-      kubectl
-      kubernetes-helm
-      libimobiledevice # usb drivers for apple mobile devices
-      mesa
-      mesa.drivers
-      moonlight-qt
-      networkmanagerapplet
-      nixos-anywhere
-      nodejs_22
-      parsec-bin
-      pavucontrol
-      peek
-      pkg-config
-      podman
-      protonmail-bridge
-      protonvpn-gui
-      python3
-      remmina
-      samba
-      screenkey
-      swaynotificationcenter
-      teams-for-linux
-      transmission_4
-      usbutils
-      # vagrant # disabled until fix is released to nixpkgs unstable
-      virt-manager
-      vlc
-      wireguard-tools
-      xournalpp
-    ];
+    environment = {
+      sessionVariables.GSK_RENDERER = "gl"; # Fix GTK apps : https://github.com/NixOS/nixpkgs/issues/353990
+      systemPackages = with pkgs; [
+        adwaita-icon-theme
+        android-tools
+        buildah
+        calibre
+        deluge-gtk
+        #discord #broken for aarch64
+        displaylink
+        element-desktop
+        evolution
+        gimp#-with-plugins # broken for aarch64
+        gnupg
+        go
+        helmfile
+        hugo
+        hyprpicker
+        ifuse # optional, to mount using 'ifuse'
+        inkscape
+        krita
+        kubectl
+        kubernetes-helm
+        libimobiledevice # usb drivers for apple mobile devices
+        mesa
+        mesa.drivers
+        moonlight-qt
+        networkmanagerapplet
+        nixos-anywhere
+        nodejs_22
+        parsec-bin
+        pavucontrol
+        peek
+        pkg-config
+        podman
+        protonmail-bridge
+        protonvpn-gui
+        python3
+        remmina
+        samba
+        screenkey
+        swaynotificationcenter
+        teams-for-linux
+        transmission_4
+        usbutils
+        # vagrant # disabled until fix is released to nixpkgs unstable
+        virt-manager
+        vlc
+        wireguard-tools
+        xournalpp
+      ];
+    };
 
     # Apple usb
     services.usbmuxd.enable = true;
