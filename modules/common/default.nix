a8706b5060e60c1d385f8aec2e5dffdb5fd92c7b
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-05-24 18:46:11 +0200

    feat: add common modules

---
+++ modules/common/default.nix
@@ -0,0 +1,10 @@
+{
+  ...
+}:
+{
+  imports = [
+    ./fonts.nix
+    ./home-manager.nix
+    ./stylix.nix
+  ];
+}
---
+++ modules/nix-darwin/desktop.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  ...
+}:
+{
+  options.desktop = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = lib.mdDoc "Whether to enable desktop-specific config";
+    };
+    environment = lib.mkOption {
+      type = lib.types.enum [ "macos" ];
+      default = "macos";
+      description = lib.mdDoc "Which Desktop Environment to install (macos)";
+    };
+  };
+
+}
