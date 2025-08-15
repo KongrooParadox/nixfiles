13005441d3ba45a1979d40c48fa7a69377eea3da
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-08-16 00:36:20 +0200

    gnome options moved for unstable

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -4,23 +4,40 @@
   pkgs,
   ...
 }:
-{
-  config = lib.mkIf (config.desktop.enable && (config.desktop.environment == "gnome")) {
-    services = {
-      gnome.gnome-browser-connector.enable = true;
-      xserver = {
-        enable = true;
-        displayManager.gdm = {
-          enable = true;
-          wayland = true;
-        };
-        desktopManager.gnome.enable = true;
-      };
+let
+  isUnstable = lib.versions.majorMinor lib.version == "25.11";
+  gnomeCfg = {
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
     };
-
-    environment.systemPackages = with pkgs; [
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.open-bar
-    ];
+    desktopManager.gnome.enable = true;
   };
+in
+{
+  config = lib.mkIf (config.desktop.enable && (config.desktop.environment == "gnome")) (
+    lib.mkMerge [
+      {
+        services = {
+          xserver.enable = true;
+          gnome.gnome-browser-connector.enable = true;
+        };
+
+        environment.systemPackages = with pkgs; [
+          gnomeExtensions.dash-to-dock
+          gnomeExtensions.open-bar
+        ];
+      }
+      (
+        if isUnstable then
+          {
+            services = gnomeCfg;
+          }
+        else
+          {
+            services.xserver = gnomeCfg;
+          }
+      )
+    ]
+  );
 }
