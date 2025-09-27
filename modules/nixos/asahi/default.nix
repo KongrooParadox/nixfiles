5841bb7a0eb7f3e8f6bd1d0b95eadb326ea170fd
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-09-27 22:40:06 +0200

    add nix-community & nixos-apple silicon binary caches

--- modules/nixos/asahi/default.nix
+++ modules/nixos/asahi/default.nix
@@ -27,5 +27,14 @@ in
     powerManagement = {
       powertop.enable = lib.mkForce false;
     };
+
+    nix.settings = {
+      extra-substituters = [
+        "https://nixos-apple-silicon.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+      ];
+    };
   };
 }
