ce6b329fc92a93ef45b8714df940018e171112b9
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-09-27 22:40:06 +0200

    add nix-community & nixos-apple silicon binary caches

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -5,4 +5,13 @@
     ./home-manager.nix
     ./stylix.nix
   ];
+
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
