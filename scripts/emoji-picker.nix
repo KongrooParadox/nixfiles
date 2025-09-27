dc2cefd33b17b00ac978e9fcf3003fd58d6ef912
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-09-27 23:37:50 +0200

    bump flake inputs

--- scripts/emoji-picker.nix
+++ scripts/emoji-picker.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 pkgs.writeShellScriptBin "emoji-picker" ''
   # Get user selection via rofi from emoji file.
-  chosen=$(cat $HOME/.config/.emoji | ${pkgs.rofi-wayland}/bin/rofi -i -dmenu -config ~/.config/rofi/config-emoji.rasi | awk '{print $1}')
+  chosen=$(cat $HOME/.config/.emoji | ${pkgs.rofi}/bin/rofi -i -dmenu -config ~/.config/rofi/config-emoji.rasi | awk '{print $1}')
 
   # Exit if none chosen.
   [ -z "$chosen" ] && exit
