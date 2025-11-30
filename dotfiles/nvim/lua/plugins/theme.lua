9832d7da5b494fcc754a003ab2c314032ad30569
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-11-30 11:35:49 +0100

    format nvim config

--- dotfiles/nvim/lua/plugins/theme.lua
+++ dotfiles/nvim/lua/plugins/theme.lua
@@ -2,26 +2,25 @@
 vim.o.termguicolors = true
 
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme catppuccin]])
-        end,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
+      },
     },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        event = "VeryLazy",
-        opts = {
-            options = {
-                icons_enabled = true,
-                theme = 'auto',
-                component_separators = '|',
-                section_separators = { left = '', right = '' },
-            },
-        },
-    },
+  },
 }
-
--- dotfiles/nvim/lua/settings.lua
+++ dotfiles/nvim/lua/settings.lua
@@ -1,6 +1,7 @@
 vim.opt.list = true
 vim.opt.showbreak = "↪"
-vim.opt.listchars = { tab=" ▸", eol="↲", nbsp="␣", trail="~", extends="⟩", precedes="⟨", space="•"}
+vim.opt.listchars =
+  { tab = " ▸", eol = "↲", nbsp = "␣", trail = "~", extends = "⟩", precedes = "⟨", space = "•" }
 
 -- Search settings
 vim.opt.hlsearch = false
@@ -11,7 +12,7 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 -- Enable mouse mode
-vim.opt.mouse = ''
+vim.opt.mouse = ""
 
 vim.opt.smartindent = true
 vim.opt.breakindent = true
@@ -29,19 +30,18 @@ vim.opt.smartcase = true
 
 vim.opt.updatetime = 50
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "120"
 
 -- Set completeopt to have a better completion experience
-vim.opt.completeopt = 'menuone,noselect'
+vim.opt.completeopt = "menuone,noselect"
 
 -- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
   end,
   group = highlight_group,
-  pattern = '*',
+  pattern = "*",
 })
-
