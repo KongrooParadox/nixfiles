0d04a47a043797be7d81754506eda91d76d66824
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2025-12-09 00:00:35 +0100

    fix sops

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -8,11 +8,20 @@
 }:
 let
   isLinux = lib.strings.hasSuffix "linux" system;
+
   sopsKeyPath =
     if config.kp.impermanence.enable then
-      map (user: "/persist${config.home.homeDirectory}/.ssh/id_ed25519") users
+      map (
+        user:
+        if isLinux then
+          "/persist/home/${user}/.ssh/id_ed25519"
+        else
+          "/persist/Users/${user}/.ssh/id_ed25519"
+      ) users
     else
-      map (user: "${config.home.homeDirectory}/.ssh/id_ed25519") users
+      map (
+        user: if isLinux then "/home/${user}/.ssh/id_ed25519" else "/Users/${user}/.ssh/id_ed25519"
+      ) users
       ++ [
         "/etc/ssh/ssh_host_ed25519_key"
       ];
