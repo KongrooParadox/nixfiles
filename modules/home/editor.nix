b1f979a447316c4606e6bc3efdc640adb33981fd
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2026-04-23 12:47:43 +0200

    emacs enable option

--- modules/home/editor.nix
+++ modules/home/editor.nix
@@ -1,75 +1,86 @@
-{ pkgs, ... }:
 {
-  editorconfig = {
-    enable = true;
-    settings = {
-      "*" = {
-        end_of_line = "lf";
-        insert_final_newline = true;
-        trim_trailing_whitespace = true;
-        charset = "utf-8";
-        indent_style = "space";
-        indent_size = 4;
-      };
-      "Makefile" = {
-        indent_style = "tab";
-      };
-      "*.{nix,yaml,yml,tf,lua}" = {
-        indent_size = 2;
-      };
-      "*.md" = {
-        indent_size = 2;
-        trim_trailing_whitespace = false;
-      };
-    };
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.kp.emacs = {
+    enable = lib.mkEnableOption "Emacs";
   };
 
-  nixpkgs.config.allowUnfreePredicate = pkg: true;
-
-  home.packages = with pkgs; [
-    # ansible-language-server #unmaintained in nixpkgs
-    bash-language-server
-    docker-compose-language-service
-    dockerfile-language-server
-    gopls
-    helm-ls
-    just
-    just-formatter
-    just-lsp
-    lua-language-server
-    llvmPackages_20.clang-unwrapped
-    nixd
-    nil
-    nixfmt
-    python313Packages.python-lsp-server
-    rust-analyzer
-    terraform
-    terraform-ls
-    typescript-language-server
-    vscode-langservers-extracted
-    yaml-language-server
-  ];
-
-  programs = {
-    doom-emacs = {
+  config = {
+    editorconfig = {
       enable = true;
-      doomDir = ../../dotfiles/doom.d;
-      # Currently broken
-      # doomDir = "${config.home.homeDirectory}/.config/doom.d";
-      # doomDir = "/nix/store/3z230glrjqibydmxv1v2r612jv8bn3pj-home-manager-files/.config/doom.d";
+      settings = {
+        "*" = {
+          end_of_line = "lf";
+          insert_final_newline = true;
+          trim_trailing_whitespace = true;
+          charset = "utf-8";
+          indent_style = "space";
+          indent_size = 4;
+        };
+        "Makefile" = {
+          indent_style = "tab";
+        };
+        "*.{nix,yaml,yml,tf,lua}" = {
+          indent_size = 2;
+        };
+        "*.md" = {
+          indent_size = 2;
+          trim_trailing_whitespace = false;
+        };
+      };
     };
-    neovim = {
-      defaultEditor = true;
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
+
+    nixpkgs.config.allowUnfreePredicate = pkg: true;
+
+    home.packages = with pkgs; [
+      # ansible-language-server #unmaintained in nixpkgs
+      bash-language-server
+      docker-compose-language-service
+      dockerfile-language-server
+      gopls
+      helm-ls
+      just
+      just-formatter
+      just-lsp
+      lua-language-server
+      llvmPackages_20.clang-unwrapped
+      nixd
+      nil
+      nixfmt
+      python313Packages.python-lsp-server
+      rust-analyzer
+      terraform
+      terraform-ls
+      typescript-language-server
+      vscode-langservers-extracted
+      yaml-language-server
+    ];
+
+    programs = {
+      doom-emacs = {
+        enable = config.kp.emacs.enable;
+        doomDir = ../../dotfiles/doom.d;
+        # Currently broken
+        # doomDir = "${config.home.homeDirectory}/.config/doom.d";
+        # doomDir = "/nix/store/3z230glrjqibydmxv1v2r612jv8bn3pj-home-manager-files/.config/doom.d";
+      };
+      neovim = {
+        defaultEditor = true;
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
+      };
     };
-  };
 
-  services.emacs = {
-    enable = true;
-  };
+    services.emacs = {
+      enable = config.kp.emacs.enable;
+    };
 
-  stylix.targets.neovim.enable = false;
+    stylix.targets.neovim.enable = false;
+  };
 }
