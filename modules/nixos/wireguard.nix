c930912393b12468a6733f3e93f6514357656d94
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2026-06-28 13:24:20 +0200

    wireguard tunnel for third site

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -4,14 +4,37 @@
     enable = lib.mkOption {
       type = lib.types.bool;
       default = false;
-      description = lib.mdDoc "Whether to enable wireguard tunnels for backup site";
+      description = lib.mdDoc "Whether to enable wireguard tunnels for backup sites";
     };
   };
 
   config = lib.mkIf config.kp.wireguard.enable {
-    sops.secrets."wireguard/casa-anita" = { };
+    sops.secrets = {
+      "wireguard/blanchissage" = { };
+      "wireguard/casa-anita" = { };
+    };
 
     networking.wg-quick.interfaces = {
+      wg-blanchissage = {
+        autostart = false;
+        privateKeyFile = config.sops.secrets."wireguard/blanchissage".path;
+        address = [ "192.168.27.65/32" ];
+        dns = [ "212.27.38.253" ];
+        mtu = 1360;
+        peers = [
+          {
+            publicKey = "O/192m1j+OyVTQn05IY2n2s2fhs8CBmZi6VqDWkr7mg=";
+            endpoint = "91.162.26.75:53080";
+            allowedIPs = [
+              "0.0.0.0/0"
+              "192.168.27.64/27"
+              "192.168.3.0/24"
+              "::/0"
+            ];
+            persistentKeepalive = 25;
+          }
+        ];
+      };
       wg-casa-anita = {
         autostart = false;
         privateKeyFile = config.sops.secrets."wireguard/casa-anita".path;
